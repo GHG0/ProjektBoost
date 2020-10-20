d16a086b95c9fd7c0910cf3e75c6783e942460b1
GHG0 <ddighe@cannonschool.org>
2020-10-20 08:29:25 -0400

    date

---
+++ Project Boost/Assets/Rocket.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour{
+    // Start is called before the first frame update
+    Rigidbody rigidbody;
+    void Start(){
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+    	ProcessInput();
+        print("update");
+    }
+    private void ProcessInput(){
+    	if(Input.GetKey(KeyCode.Space)){
+    		print("Thrusting");
+    		rigidbody.AddRelativeForce(Vector3.up);
+    	}
+    	if(Input.GetKey(KeyCode.A)){
+    		print("rotate left");
+    	}
+    	else if(Input.GetKey(KeyCode.D)){
+    		print("rotate right");
+    	}
+    } 
+
+}
