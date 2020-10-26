93517d6fe5a9a74407712ac4995664b429b56130
GHG0 <ddighe@cannonschool.org>
2020-10-25 21:46:54 -0400

    level 19

--- Project Boost/Assets/Rocket.cs
+++ Project Boost/Assets/Rocket.cs
@@ -5,26 +5,54 @@ using UnityEngine;
 public class Rocket : MonoBehaviour{
     // Start is called before the first frame update
     Rigidbody rigidbody;
+    AudioSource audio;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
     void Start(){
         rigidbody = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update(){
-    	ProcessInput();
+    	rotate();
+    	thrust();
+
         print("update");
     }
-    private void ProcessInput(){
-    	if(Input.GetKey(KeyCode.Space)){
-    		print("Thrusting");
-    		rigidbody.AddRelativeForce(Vector3.up);
-    	}
+    private void rotate(){
+    	rigidbody.freezeRotation = true; //take manual controll
+    	float rotationtf = rcsThrust * Time.deltaTime;
     	if(Input.GetKey(KeyCode.A)){
-    		print("rotate left");
+    		transform.Rotate(Vector3.forward*rotationtf);
     	}
     	else if(Input.GetKey(KeyCode.D)){
-    		print("rotate right");
-    	}
-    } 
+    		transform.Rotate(-Vector3.forward*rotationtf);
 
-}
+    	}
+    	rigidbody.freezeRotation = false;
+    } 
+    private void thrust(){
+    	if(Input.GetKey(KeyCode.Space)){
+    		rigidbody.AddRelativeForce(Vector3.up*mainThrust);
+    		if(!audio.isPlaying){
+    			audio.Play();
+    		}
+    	}else{
+    		audio.Stop();
+    	}
+	}
+	void OnCollisionEnter(Collision collision){
+		switch (collision.gameObject.tag){
+			case "Friendly":
+				//do nothing
+				print("ok");
+				break;
+			default:
+				print("Dead");
+				//kill/reload
+				break;
+		}
+	}
+}//serialize mainthrust
